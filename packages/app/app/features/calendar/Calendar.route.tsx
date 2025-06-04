48d3d8abfa95d190c9d8547f7a2ff163d84b1998
Drew DeCarme <drewdecarme@gmail.com>
2025-06-04 08:57:05 -0400

    pull calendar data

--- packages/app/app/features/calendar/Calendar.route.tsx
+++ packages/app/app/features/calendar/Calendar.route.tsx
@@ -1,5 +1,34 @@
+import type { Route } from "./+types/Calendar.route";
+
 import { PageSection } from "../../components/page";
 
-export default function CalendarRoute() {
-  return <PageSection>content</PageSection>;
+export async function loader(args: Route.LoaderArgs) {
+  const url = new URL(
+    `https://www.googleapis.com/calendar/v3/calendars/${args.context.cloudflare.env.GOOGLE_CALENDAR_ID_NCCL_PUBLIC}/events`
+  );
+  url.searchParams.set(
+    "key",
+    args.context.cloudflare.env.GOOGLE_CALENDAR_API_KEY
+  );
+  url.searchParams.set("maxResults", "10");
+  url.searchParams.set("orderBy", "startTime");
+  url.searchParams.set("singleEvents", "true");
+  url.searchParams.set("timeMin", new Date().toISOString());
+
+  const res = await fetch(url);
+  const json = await res.json();
+
+  return {
+    events: json,
+  };
+}
+
+export default function CalendarRoute(args: Route.ComponentProps) {
+  return (
+    <PageSection dxTitle="Data for the next 10 google cal events">
+      <code style={{ width: "80%", overflow: "auto" }}>
+        {JSON.stringify(args.loaderData.events, null, 2)}
+      </code>
+    </PageSection>
+  );
 }
