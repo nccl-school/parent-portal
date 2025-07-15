f7b2da4867461a5808c845bf33f3976c81ed6066
Drew DeCarme <drewdecarme@gmail.com>
2025-07-15 11:36:48 -0400

    log dir

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -22,8 +22,6 @@ dotenv.config({ path: envPath });
 
 const app = new Hono();
 
-console.log(process.env);
-
 // Middleware - Log and add the db to the context
 app.use(logger());
 app.use("/api/*", prismaMiddleware);
