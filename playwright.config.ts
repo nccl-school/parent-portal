59d5beaaba4b9b7829c904ef4a16b071ea0e6691
Drew DeCarme <drewdecarme@gmail.com>
2025-07-17 09:46:25 -0400

    updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,12 @@ import path from "path";
 import { defineConfig } from "@playwright/test";
 import dotenv from "dotenv";
 
-dotenv.config({ path: path.resolve(import.meta.dirname, ".env.spec") });
+dotenv.config({
+  path: [
+    path.resolve(import.meta.dirname, "./.env.spec"),
+    path.resolve(import.meta.dirname, "./.env"),
+  ],
+});
 
 /**
  * See https://playwright.dev/docs/test-configuration.
