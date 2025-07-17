3d82d290271d66e95afddc7bbe7a2ecda36b3318
Drew DeCarme <drewdecarme@gmail.com>
2025-07-17 11:05:01 -0400

    remove env logging

--- packages/api/prisma.config.ts
+++ packages/api/prisma.config.ts
@@ -4,8 +4,6 @@ import { loadEnv } from "./scripts/load-env.script.js";
 
 loadEnv();
 
-console.log(process.env);
-
 export default {
   earlyAccess: true,
   // now you can use process.env variables
