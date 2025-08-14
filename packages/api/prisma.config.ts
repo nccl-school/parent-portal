c857ad39fcf5874f357e255d8c7b4340c15748c1
Drew DeCarme <drewdecarme@gmail.com>
2025-08-14 13:24:00 -0400

    remove seed from pckjson

--- packages/api/prisma.config.ts
+++ packages/api/prisma.config.ts
@@ -5,5 +5,8 @@ import { loadEnv } from "./scripts/load-env.script.js";
 loadEnv();
 
 export default {
+  migrations: {
+    seed: "tsx ./prisma/seed.ts",
+  },
   // now you can use process.env variables
 } satisfies PrismaConfig;
