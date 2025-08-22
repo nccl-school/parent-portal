74fac250935d24079a7714c24a23e22262e855e6
Drew DeCarme <drewdecarme@gmail.com>
2025-08-22 11:28:15 -0400

    some updates

--- packages/api/prisma.config.ts
+++ packages/api/prisma.config.ts
@@ -1,17 +1,17 @@
-import path from "node:path";
-
 import type { PrismaConfig } from "prisma";
-import { ENV_RUNTIME } from "@nccl/env";
+import { ENV_RUNTIME, validateCI } from "@nccl/env";
+
+if (ENV_RUNTIME.getOne("NCCL_ENVIRONMENT") === "test") {
+  validateCI();
+}
 
 // Just ensure that the DB URL has been set since that's
 // all we need here for the the migrations and the
 // client generation to work
-ENV_RUNTIME.load({ paths: [path.resolve(import.meta.dirname, "../../.env")] });
 ENV_RUNTIME.getOne("DATABASE_URL");
 
 export default {
   migrations: {
     seed: "tsx ./prisma/seed.ts",
   },
-  // now you can use process.env variables
 } satisfies PrismaConfig;
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -139,3 +139,36 @@ export const ENV_RUNTIME = new Supermenv({
 // TODO: Get tests working locally
 // TODO: Cleanup scripts to test
 // TODO: Separate out the scripts into their own directories
+
+export function validateCI() {
+  // Validate CI right out of the gate for caching purposes
+  ENV_CI.validate();
+
+  // Set some variables based upon known values
+  // and some implicit env vars
+  const db = "nccl-parents-db-test";
+  const dbPort = 11002;
+  const user = "postgres";
+  const pw = ENV_TEST.getOne("POSTGRES_PASSWORD");
+  const appPort = 11000;
+  const apiPort = 11001;
+
+  // Set some of them
+  ENV_TEST.set("APP_PORT", appPort);
+  ENV_TEST.set("API_PORT", apiPort);
+  ENV_TEST.set("POSTGRES_DB", db);
+  ENV_TEST.set("POSTGRES_PORT", dbPort);
+  ENV_TEST.set("POSTGRES_USER", user);
+
+  // Dynamically set some runtime URLs based upon
+  // the derived env vars
+  const DATABASE_URL = `postgresql://${user}:${pw}@db:${dbPort}/${db}`;
+  ENV_RUNTIME.set("DATABASE_URL", DATABASE_URL);
+  ENV_RUNTIME.set("NCCL_APP_URL", `http://app:${appPort}`);
+  ENV_RUNTIME.set("NCCL_API_URL", `http://app:${apiPort}`);
+
+  // Validate that the dynamically composed TEST vars
+  // have successfully been set and loaded
+  ENV_RUNTIME.validate();
+  ENV_TEST.validate();
+}
