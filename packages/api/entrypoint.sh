b88cf0c3ea106eaef110b6ccfcd61c34b6619c47
Drew DeCarme <drewdecarme@gmail.com>
2025-08-25 17:10:27 -0400

    add migrate service to docker

--- packages/api/entrypoint.sh
+++ packages/api/entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
 
-echo "🏃 Running Prisma migrations..."
-npx prisma migrate deploy --schema /app/prisma/schema.prisma
-
-echo "🌱 Seeding DB..."
-npx prisma db seed --config /app/prisma.config.ts
-
 echo "🚀 Starting API server..."
 exec node /app/dist/index.js
---
+++ packages/api/entrypoint.spec-migrate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "🏃 Running Prisma migrations..."
+npx prisma migrate deploy --schema /app/prisma/schema.prisma
+
+echo "🌱 Seeding DB..."
+npx prisma db seed --config /app/prisma.config.ts
