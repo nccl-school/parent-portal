75407960a85063b0378b62502566e0b5825f019e
Drew DeCarme <drewdecarme@gmail.com>
2025-08-29 06:08:03 -0400

    fix some bugs

--- packages/app/app/features/auth/AuthOAuthError.route.tsx
+++ packages/app/app/features/auth/AuthOAuthError.route.tsx
@@ -1,4 +1,4 @@
-import { Button, Callout, Typography } from "@nccl/components";
+import { Button } from "@nccl/components";
 import { useNavigate, href, useSearchParams } from "react-router";
 
 import { AuthPage } from "./AuthPage";
@@ -29,25 +29,11 @@ export default function AuthResetPasswordSuccess() {
             imgAlt="alert"
             borderless
             title={urlSearchParams.get("error") ?? "Unknown error"}
-          />
-          <Typography dxNode="div" dxVariant="body1">
-            This could be for one of many reasons
-            <ul>
-              <li>You have not been invited to the platform</li>
-              <li>
-                You have been invited but you have not accepted your invite
-                which was sent to your inbox
-              </li>
-              <li>
-                You're attempting to accept your invite but it has expired
-              </li>
-              <li>
-                You tried to sign in with an using an email that wasn't
-                explicitly invited.
-              </li>
-            </ul>
-            Either way, click on the button below and try again.
-          </Typography>
+          >
+            You either have not been invited, you have not accepted your invite
+            or there was an error during the social login process. If you feel
+            that it was the former, please reach out to the administrator.
+          </EmptyState>
         </AuthPageBody>
         <AuthPageFooter>
           <Button
