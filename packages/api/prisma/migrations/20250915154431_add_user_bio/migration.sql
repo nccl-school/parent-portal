9ce3f9e51ec7ec8551e9e69a3432edc63532fcd3
Drew DeCarme <drewdecarme@gmail.com>
2025-09-15 11:45:44 -0400

    adds bio to schema

---
+++ packages/api/prisma/migrations/20250915154431_add_user_bio/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrlOriginal` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."user" DROP COLUMN "imageUrlOriginal",
+ADD COLUMN     "bio" TEXT;
